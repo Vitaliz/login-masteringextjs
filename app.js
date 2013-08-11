31c893d9ba2598ac9cbd4781af9850e44f55358b
Nadjib Amar <hello@ebabel.eu>
2013-08-11 19:04:14 +0200

    UPDATED validation on password.

--- app.js
+++ app.js
@@ -11,8 +11,24 @@ Ext.application({
     name: 'Mej2',
 
     extend: 'Mej2.Application',
+    
+    requires: [
+        'Ext.form.field.VTypes'
+    ],
 
     launch: function () {
+        // Custom validation for password fields.
+        // todo: refactor in its own js file.
+        Ext.apply(Ext.form.field.VTypes, {
+            customPass: function (value, field) {
+                return /^((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/.test(value);
+            },
+            customPassText: 'Not a valid password. ' +
+            'Password must contain one digit, one letter lowercase, one letter uppercase, ' + 
+            'one special symbol @#$% and between 6 and 20 characters.'
+        });
+        
+        // Open the login.
         Ext.widget('login');
     }
 });
